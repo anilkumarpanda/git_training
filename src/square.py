d6ccf3b615b888fea18fc7bbe9894fc2e8c457a7
anilkumarpanda <anilkumarpanda@users.noreply.github.com>
2019-01-04 16:11:00 +0100

    Learning to collaborate

--- src/square.py
+++ src/square.py
@@ -1,7 +1,16 @@
 #Code to calculate square of a given integer
 def square(x):
-"""
-This function creates the square of given
-integer
-"""
+ """
+ This function creates the square of given
+ integer
+ """
  return x*x
+ 
+def cube(x):
+ 
+ """
+ 
+ :param x: 
+ :return: cube of a number
+ """
+ return x*x*x
